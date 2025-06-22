d1fd30bb1a8b5048588dc8b637c63e6c3adc8c2a
Josh Wycuff <josh.wycuff@astronomer.io>
2025-06-21 23:31:16 -0400

    update example-go formula

--- Formula/example-go.rb
+++ Formula/example-go.rb
@@ -7,9 +7,9 @@ class ExampleGo < Formula
   def install
     bin.install "example-go"
 
-    system bin/"example-go", "man"
-    man1.install Dir["*.1"]
-
-    generate_completions_from_executable(bin/"example-go", "completion")
+#     system bin/"example-go", "man"
+#     man1.install Dir["*.1"]
+#
+#     generate_completions_from_executable(bin/"example-go", "completion")
   end
 end
