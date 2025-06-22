cf92a3893d0cd4a7b11fa346c3a4d214d90a8d14
Josh Wycuff <josh.wycuff@astronomer.io>
2025-06-21 22:26:15 -0400

    trigger update

--- Formula/fman.rb
+++ Formula/fman.rb
@@ -1,8 +1,8 @@
 class Fman < Formula
   desc "Perform fuzzy search on man pages."
   homepage "https://github.com/joshwycuff/homebrew-toolbox"
-  url "https://github.com/joshwycuff/homebrew-toolbox/releases/download/fman-b185f0ccd960959ee20ad2c2288a06d01da81191/fman",
-  version "b185f0ccd960959ee20ad2c2288a06d01da81191"
+  url "https://github.com/joshwycuff/homebrew-toolbox/releases/download/fman-b185f0ccd960959ee20ad2c2288a06d01da8119/fman",
+  version "b185f0ccd960959ee20ad2c2288a06d01da8119"
 
   depends_on "apropos"
   depends_on "fzf"
