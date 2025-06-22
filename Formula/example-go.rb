24dcbf0392c3c6d0f48198828a82e85c254eb121
Josh Wycuff <josh.wycuff@astronomer.io>
2025-06-21 23:57:47 -0400

    fix download url

--- Formula/example-go.rb
+++ Formula/example-go.rb
@@ -1,7 +1,7 @@
 class ExampleGo < Formula
   desc "A brief description of your application"
   homepage "https://github.com/joshwycuff/homebrew-toolbox"
-  url "https://github.com/joshwycuff/homebrew-toolbox/releases/download/example-go-8541924f0e195adaf1f5ae90f8f72572e7e92712/example-go-8541924f0e195adaf1f5ae90f8f72572e7e92712.tar.gz"
+  url "https://github.com/joshwycuff/homebrew-toolbox/archive/refs/tags/example-go-8541924f0e195adaf1f5ae90f8f72572e7e92712.tar.gz"
   version "8541924f0e195adaf1f5ae90f8f72572e7e92712"
 
   def install
